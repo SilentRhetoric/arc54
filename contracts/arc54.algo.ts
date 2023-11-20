161555cd8cdb668185fc7958ecb760f036ee23a1
SilentRhetoric <bwhippo@gmail.com>
2023-11-19 21:52:16 -0500

    Simplified contract

--- contracts/arc54.algo.ts
+++ contracts/arc54.algo.ts
@@ -1,47 +1,22 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
-class Arc54 extends Contract {
-  /**
-   * Calculates the sum of two numbers
+class ARC54 extends Contract {
+  /*
+   * Sends an inner transaction to opt the contract account into an ASA.
+   * Will fail if the contract is already opted in to the asset.
+   * The fee for the inner transaction must be covered by the caller.
    *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
+   * @param asa The ASA to opt in to
    */
-  private getSum(a: number, b: number): number {
-    return a + b;
-  }
+  optIntoASA(asa: Asset): void {
+    assert(!globals.currentApplicationAddress.hasAsset(asa));
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: number, b: number): number {
-    return a >= b ? a - b : b - a;
-  }
-
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first number
-   * @param b The second number
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: number, b: number, operation: string): number {
-    let result: number;
-
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
-
-    return result;
+    sendAssetTransfer({
+      assetReceiver: globals.currentApplicationAddress,
+      xferAsset: asa,
+      assetAmount: 0,
+      fee: 0,
+    });
   }
 }
